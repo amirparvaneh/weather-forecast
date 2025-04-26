9da12027a4d97696943049b677cf073beaf91820
a.parvaneh <amirparvaneh138@gmail.com>
2025-04-26 23:30:44 +0330

    [feature] unit test

--- src/main/java/com/weatherforecast/model/response/WeatherApiResponse.java
+++ src/main/java/com/weatherforecast/model/response/WeatherApiResponse.java
@@ -3,9 +3,11 @@ package com.weatherforecast.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class WeatherApiResponse {
     private Location location;
     private CurrentWeather current;
